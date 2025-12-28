9d112931a2154ec4671f150690dbd27dea5f079f
Shalom Sayag <Shalom.s@eitanmedical.com>
2025-12-28 11:05:03 +0200

    HTML changes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 st.title("My Site")
 st.subheader("Welcome to my site!")
+st.write("Testing simple HTML <b>bold</b> and <i>italic</i> text rendering.", unsafe_allow_html=True)
 
 
 st.checkbox("Subscribe to newsletter")
