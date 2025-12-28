295b703c21dc791a7472fa95638adf2b72e7e3a2
Shalom Sayag <Shalom.s@eitanmedical.com>
2025-12-28 10:19:17 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("My Site")
+st.subheader("Welcome to my site!")
+
+
+st.checkbox("Subscribe to newsletter")
+st.text_input("Your email address", placeholder="type here")
